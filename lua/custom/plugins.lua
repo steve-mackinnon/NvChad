85c096e4248611615f574b73df7d4e1d88474e4c
Steve MacKinnon <steve@Steves-MacBook-Pro.local>
2023-09-26 21:20:03 -0400

    Add clang-format

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,5 +1,12 @@
 local plugins = {
   {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
