4c55174154de733b0424a6370bd7b90898bf1e00
Steve MacKinnon <stephen.mackinnon@splice.com>
2023-11-16 22:46:49 -0500

    Dont use detour for gd

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -132,7 +132,6 @@ M.lspconfig = {
   n = {
     ["gD"] = {
       function()
-        require('detour').Detour()
         vim.lsp.buf.declaration()
       end,
       "LSP declaration",
@@ -140,7 +139,6 @@ M.lspconfig = {
 
     ["gd"] = {
       function()
-        require('detour').Detour()
         vim.lsp.buf.definition()
       end,
       "LSP definition",
