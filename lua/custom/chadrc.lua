7db1a306960c6567751eaacebc055cbb928076ea
Steve MacKinnon <mackinnon.st@gmail.com>
2024-01-21 20:22:50 -0500

    Use fish shell

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -7,7 +7,10 @@ M.mappings = require "custom.mappings"
 
 vim.opt.expandtab = true
 vim.opt.relativenumber = true
+vim.opt.shell = "fish"
 
-vim.opt.guicursor={"n-v-c:block,i-ci-ve:ver10,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"}
+vim.opt.guicursor = {
+  "n-v-c:block,i-ci-ve:ver10,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
+}
 
 return M
