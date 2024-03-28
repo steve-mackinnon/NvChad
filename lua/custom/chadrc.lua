bc9135fb939e4ca8d0214746e287f5bb1bc4ae70
Steve MacKinnon <mackinnon.st@gmail.com>
2024-03-28 16:13:40 -0400

    copy to clipboard

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,7 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = "catppuccin" }
+M.ui = { theme = "wombat" }
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
@@ -11,6 +11,8 @@ vim.opt.shell = "fish"
 
 vim.opt.listchars = "tab:>-"
 vim.opt.list = true
+
+vim.opt.clipboard:append("unnamedplus")
 
 vim.opt.guicursor = {
   "n-v-c:block,i-ci-ve:ver10,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
