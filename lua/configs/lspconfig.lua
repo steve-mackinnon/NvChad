bec57bed67f2413c26e998a6753a38564084a5b4
Steve MacKinnon <mackinnon.st@gmail.com>
2026-06-05 22:22:15 -0400

    Add ts lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "gopls" }
+local servers = { "html", "cssls", "ts_ls", "gopls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -24,6 +24,35 @@ return {
   -- { import = "nvchad.blink.lazyspec" },
 
   {
+    "mason-org/mason.nvim",
+    event = "VeryLazy", -- load at startup so ensure_installed runs
+    opts = function()
+      local opts = require "nvchad.configs.mason" -- nvchad ui/defaults
+      opts.ensure_installed = {
+        "lua-language-server", "stylua",
+        "html-lsp", "css-lsp", -- html, cssls
+        "typescript-language-server",
+        "gopls",
+      }
+      return opts
+    end,
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      -- mason 2.0 core has no ensure_installed; install missing packages
+      local registry = require "mason-registry"
+      registry.refresh(function()
+        for _, name in ipairs(opts.ensure_installed or {}) do
+          local ok, pkg = pcall(registry.get_package, name)
+          if ok and not pkg:is_installed() then
+            pkg:install()
+          end
+        end
+      end)
+    end,
+  },
+
+  {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
     build = ":TSUpdate",
@@ -31,6 +60,7 @@ return {
       ensure_installed = {
         "vim", "lua", "vimdoc",
         "html", "css",
+        "javascript", "typescript", "tsx",
         "go", "gomod", "gosum", "gotmpl",
       },
     },
