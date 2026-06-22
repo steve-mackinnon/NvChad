cf3c0ae4e5a7131d825c9f8cd9c057d5265e8318
Steve MacKinnon <steve@unchartedlabs.ai>
2026-06-22 17:31:06 -0400

    Add gitlinker

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -19,6 +19,16 @@ return {
   },
 
   {
+    "linrongbin16/gitlinker.nvim",
+    cmd = "GitLink",
+    opts = {},
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<cr>", mode = { "n", "v" }, desc = "Copy git permalink to clipboard" },
+      { "<leader>go", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git permalink in browser" },
+    },
+  },
+
+  {
     "sindrets/diffview.nvim",
     dependencies = "nvim-lua/plenary.nvim",
     cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles", "DiffviewFileHistory" },
