08449140b1106daa4c3a2009b41578e226ae6e64
Steve MacKinnon <steve@unchartedlabs.ai>
2026-06-26 14:46:09 -0400

    MRU buffer swap with leader tab

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,12 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- VS Code style Ctrl+Tab: MRU open-buffer picker
+map("n", "<leader><Tab>", function()
+  require("telescope.builtin").buffers {
+    sort_mru = true,
+    ignore_current_buffer = true,
+  }
+end, { desc = "Telescope MRU buffers" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
